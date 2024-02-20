f1af014b69aea582bc0178f582259f75be3b1cb3
L?101111 <159942427+L101111@users.noreply.github.com>
2024-02-20 16:07:43 +0400

    Update NetVision.py

--- NetVision.py
+++ NetVision.py
@@ -2,13 +2,12 @@ import socket
 import sys
 from termcolor import colored, cprint
 
-try:
 
-    ex1=colored('$ python3 NetVision.py 192.168.126.129 21/1000', 'blue')
-    ex2=colored('$ python3 NetVision.py 192.168.126.129 -p-', 'blue')
-    ex3=colored('Ctrl+C', 'red')
-    logo=colored(''' 
-
+def print_help():
+    ex1 = colored('$ python3 NetVision.py 192.168.126.129 21/1000', 'blue')
+    ex2 = colored('$ python3 NetVision.py 192.168.126.129 -p-', 'blue')
+    ex3 = colored('Ctrl+C', 'red')
+    logo = colored(''' 
                      | \ | | ___| |\ \   / (_)___(_) ___  _ __  
                      |  \| |/ _ \ __\ \ / /| / __| |/ _ \| '_ \ 
                      | |\  |  __/ |_ \ V / | \__ \ | (_) | | | |
@@ -16,8 +15,8 @@ try:
                                             Made by hei$enberg  
 
     ''', 'blue')
-    if sys.argv[1]=='-h' or sys.argv[1]=='--help' or sys.argv[1]=='help':
-        print(f'''                       
+
+    print(f'''                       
         {logo}
                     Welcome to NetVision Network Scanner!
 
@@ -34,9 +33,10 @@ try:
         To exit, simply press {ex3}
 
             
-            An example of a scan:
-            ''')
-        cprint(''' 
+        An example of a scan:
+    ''')
+
+    cprint(''' 
         ***********************************
         SCAN RESULTS FOR 192.168.126.129
          
@@ -55,103 +55,108 @@ try:
                 scan complete
 
             ''', 'yellow')
-        sys.exit()
 
-    ports_and_protocols = [
-        20, 'FTP',
-        21, 'FTP',
-        22, 'SSH',
-        23, 'Telnet',
-        25, 'SMTP',
-        53, 'DNS',
-        67, 'DHCP Server',
-        68, 'DHCP Client',
-        80, 'HTTP',
-        110, 'POP3',
-        119, 'NNTP',
-        123, 'NTP',
-        143, 'IMAP',
-        161, 'SNMP',
-        194, 'IRC',
-        443, 'HTTPS',
-        465, 'SMTPS',
-        993, 'IMAPS',
-        995, 'POP3S',
-        1433, 'MSSQL',
-        3306, 'MySQL',
-        3389, 'RDP',
-        5432, 'PostgreSQL',
-        5900, 'VNC',
-    ]
-
-    ip_addr = sys.argv[1]
-    ports = sys.argv[2]
-    open_p = []
-
+ports_and_protocols = {
+        20: 'FTP',
+        21: 'FTP',
+        22: 'SSH',
+        23: 'Telnet',
+        25: 'SMTP',
+        53: 'DNS',
+        67: 'DHCP Server',
+        68: 'DHCP Client',
+        80: 'HTTP',
+        110: 'POP3',
+        119: 'NNTP',
+        123: 'NTP',
+        143: 'IMAP',
+        161: 'SNMP',
+        194: 'IRC',
+        443: 'HTTPS',
+        465: 'SMTPS',
+        993: 'IMAPS',
+        995: 'POP3S',
+        1433: 'MSSQL',
+        3306: 'MySQL',
+        3389: 'RDP',
+        5432: 'PostgreSQL',
+        5900: 'VNC',
+    }
+def scan_ports(ip_addr, ports):
     cprint("""
 
 
-    ███╗   ██╗███████╗████████╗██╗   ██╗██╗███████╗██╗ ██████╗ ███╗   ██╗
-    ████╗  ██║██╔════╝╚══██╔══╝██║   ██║██║██╔════╝██║██╔═══██╗████╗  ██║
-    ██╔██╗ ██║█████╗     ██║   ██║   ██║██║███████╗██║██║   ██║██╔██╗ ██║
-    ██║╚██╗██║██╔══╝     ██║   ╚██╗ ██╔╝██║╚════██║██║██║   ██║██║╚██╗██║
-    ██║ ╚████║███████╗   ██║    ╚████╔╝ ██║███████║██║╚██████╔╝██║ ╚████║
-    ╚═╝  ╚═══╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝
-                                                       Made by hei$enberg
+███╗   ██╗███████╗████████╗██╗   ██╗██╗███████╗██╗ ██████╗ ███╗   ██╗
+████╗  ██║██╔════╝╚══██╔══╝██║   ██║██║██╔════╝██║██╔═══██╗████╗  ██║
+██╔██╗ ██║█████╗     ██║   ██║   ██║██║███████╗██║██║   ██║██╔██╗ ██║
+██║╚██╗██║██╔══╝     ██║   ╚██╗ ██╔╝██║╚════██║██║██║   ██║██║╚██╗██║
+██║ ╚████║███████╗   ██║    ╚████╔╝ ██║███████║██║╚██████╔╝██║ ╚████║
+╚═╝  ╚═══╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝
+                                                   Made by hei$enberg
 
                                    
-    """, "blue")
+""", "blue")
+    
+    open_ports = []
 
-
-
-    if ports!='-p-':
+    if ports != '-p-':
         try:
-            ports0 = ports.split('/')
-            port_min = int(ports0[0])
-            port_max = int(ports0[1])
+            port_range = ports.split('/')
+            port_min = int(port_range[0])
+            port_max = int(port_range[1])
         except (ValueError, IndexError):
             port_min = port_max = int(ports)
-
-        
-
-        for port in range(port_min, port_max + 1):
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as xndzor:
-                xndzor.settimeout(0.5)
-                try:
-                    xndzor.connect((ip_addr, port))
-                    open_p.append(port)
-                except (ConnectionRefusedError, TimeoutError, Exception):
-                    pass
     else:
-        for port in range(1,65536):
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as xndzor:
-                xndzor.settimeout(0.5)
-                try:
-                    xndzor.connect((ip_addr, port))
-                    open_p.append(port)
-                except (ConnectionRefusedError, TimeoutError, Exception):
-                    pass
+        port_min = 1
+        port_max = 65535
 
-    if len(open_p)>0:
+    for port in range(port_min, port_max + 1):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(0.5)
+            try:
+                s.connect((ip_addr, port))
+                open_ports.append(port)
+            except (ConnectionRefusedError, TimeoutError):
+                pass
+
+    return open_ports
+
+
+def print_scan_results(ip_addr, open_ports):
+    if open_ports:
         cprint(f'SCAN RESULTS FOR {ip_addr}', 'yellow')
         print(' ')
         cprint("{:<10} {:<15} {:<10}".format('PORT', 'PROTOCOL', 'STATE'), 'blue')
         print(' ')
-        for port in open_p:
-            for i in range(0, len(ports_and_protocols), 2):
-                if ports_and_protocols[i] == port:
-                    pr=ports_and_protocols[i + 1]
-            print("{:<10} {:<15} {:<10}".format(port, pr, 'OPEN'))
+        for port in open_ports:
+            protocol = ports_and_protocols.get(port, 'Unknown')
+            print("{:<10} {:<15} {:<10}".format(port, protocol, 'OPEN'))
         print(' ')
         cprint('    scan complete', 'blue')
     else:
         print(' ')
-        cprint('    no ports were found:( ', 'red')
-except IndexError:
-    print(' ')
-    cprint('    wrong parameters...', 'red')
+        cprint('    no ports were found :( ', 'red')
 
-except KeyboardInterrupt:
-    print(' ')
-    cprint('    exiting ...', 'red')
+def main():
+    try:
+        if len(sys.argv) == 2 and (sys.argv[1] == '-h' or sys.argv[1] == '--help' or sys.argv[1] == 'help'):
+            print_help()
+            sys.exit()
+        elif len(sys.argv) != 3:
+            raise IndexError
 
+        ip_addr = sys.argv[1]
+        ports = sys.argv[2]
+
+        open_ports = scan_ports(ip_addr, ports)
+        print_scan_results(ip_addr, open_ports)
+    except IndexError:
+        print(' ')
+        cprint('    wrong parameters...', 'red')
+    except KeyboardInterrupt:
+        print(' ')
+        cprint('    exiting ...', 'red')
+
+
+if __name__ == "__main__":
+    main()
